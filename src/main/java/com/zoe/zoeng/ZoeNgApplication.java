96ae72863b286c6cb998515bee476f63965b3b5f
LollipopZoo <zoe.ek.ng@gmail.com>
2026-05-07 14:04:08 -0400

    initial

---
+++ src/main/java/com/zoe/zoeng/ZoeNgApplication.java
@@ -0,0 +1,13 @@
+package com.zoe.zoeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoeNgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoeNgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoe/zoeng/ZoeNgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoe.zoeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoeNgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
