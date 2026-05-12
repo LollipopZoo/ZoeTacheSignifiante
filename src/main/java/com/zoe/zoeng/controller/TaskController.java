8cd342358de1b2c430ae6c8dcf61dc0eab8567f0
LollipopZoo <zoe.ek.ng@gmail.com>
2026-05-12 14:00:51 -0400

    Partie 1

---
+++ src/main/java/com/zoe/zoeng/controller/TaskController.java
@@ -0,0 +1,12 @@
+package com.zoe.zoeng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/zoe/zoeng/model/Task.java
@@ -0,0 +1,62 @@
+package com.zoe.zoeng.model;
+
+import java.time.LocalDate;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private boolean completed;
+
+    public Task() {
+    }
+
+    public Task(int id, String title, String description, LocalDate dueDate, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
